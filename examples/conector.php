4367a659f89f7decf3711e22c4df02939e87055d
Guille <guille_55_88@hotmail.com>
2017-06-24 18:35:43 -0500

    change examples tinymce 4.1.10 to tinymce 4.6.4

---
+++ examples/conector.php
@@ -0,0 +1,23 @@
+<?php
+include("../../filemanager-php/vendor/autoload.php");
+use GuillermoMartinez\Filemanager\Filemanager;
+
+// Add your own authentication method
+//if(!isset($_SESSION['username']) || $_SESSION['username']!="")
+//  exit();
+$extra = array(
+    // path after of root folder
+    // if /var/www/public_html is your document root web server
+    // then source= usefiles o filemanager/usefiles
+    "source" => "github/filemanager-php/userfiles",
+    // url domain
+    // so that the files and show well http://php-filemanager.rhcloud.com/userfiles/imagen.jpg
+    // o http://php-filemanager.rhcloud.com/filemanager/userfiles/imagen.jpg
+    "url" => "http://localhost/"
+    );
+if(isset($_POST['typeFile']) && $_POST['typeFile']=='images'){
+    $extra['type_file'] = 'images';
+}
+$f = new Filemanager($extra);
+$f->run();
+?>
